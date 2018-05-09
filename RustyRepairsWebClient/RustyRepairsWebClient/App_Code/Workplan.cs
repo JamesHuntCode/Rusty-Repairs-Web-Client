356d934a748310537465c5a0f3ecac55dca08a2f
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-05-09 19:56:38 +0100

    work plan class coded

--- RustyRepairsWebClient/RustyRepairsWebClient/App_Code/Workplan.cs
+++ RustyRepairsWebClient/RustyRepairsWebClient/App_Code/Workplan.cs
@@ -2,6 +2,14 @@
 {
     public class Workplan
     {
+        public int WorkPlanID { get; set; }
+
+        public string DateStarted { get; set; }
+        public string EstimatedCompletionDate { get; set; }
+        public string ActualCompletionDate { get; set; }
+
+        public Staff AssignedStaffMember { get; set; }
+
         public Workplan()
         {
             
